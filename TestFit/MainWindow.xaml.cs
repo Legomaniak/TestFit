5a215378af4f90be77947ae305c1fc7e344a994d
Legomaniak <jiricek.cechu@seznam.cz>
2021-04-16 14:50:07 +0200

    Planc

--- TestFit/MainWindow.xaml.cs
+++ TestFit/MainWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace TestFit
             get { return _MaxIter; }
             set { _MaxIter = value; OnPropertyChanged("MaxIter"); }
         }
-        private double _Gradient = 1;
+        private double _Gradient = 0.01;
 
         public double Gradient
         {
@@ -70,45 +70,91 @@ namespace TestFit
             set { _Results = value; OnPropertyChanged("Results"); }
         }
 
+        public double TempK0 = -273.15;
+        double StefanBoltzmannKonst = 5.670373 * Math.Pow(10, -8);
+        double c0 = 299792458;//m/s
+        double kb = 1.38064852 * Math.Pow(10, -23); //J/K
+        double h = 6.626070040 * Math.Pow(10, -34); //J.s
+        double c1 = 0;
+        double c2 = 0;
+        Vector<double> W = null;
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             show.showName = false;
 
-            //BM = new MyConjugateGradientMinimizer(Gradient, MaxIter);
-            BM = new NelderMeadSimplex(Gradient, MaxIter);
-            //MyFuncE = ObjectiveFunction.Gradient(MinimizeE, MinimizeG);
-            MyFuncE = ObjectiveFunction.Value(MinimizeE);
+            c1 = 2 * h * Math.Pow(c0, 2); //W-um**4/m**2
+            c2 = h * c0 / kb; //um-K
 
-            Random r = new Random();
-            var N = 100;
-            double A = 5.1, B = 1.8;
-            var s0 = new double[N];
-            var s1 = new double[N];
-            for (int i = 0; i < N; i++)
-            {
-                s0[i] = i + (r.Next(2)-1.0) / 2.0;
-                s1[i] = A + B * i + (r.Next(2) - 1.0) * B / 2.0;
-            }
-            sample0 = CreateVector.DenseOfArray(s1);
-            sample1 = CreateVector.DenseOfArray(s0);
             StartFit();
         }
 
         public void StartFit()
         {
+            Random r = new Random();
+            var N = 100;
+            switch (cbM.SelectedIndex)
+            {
+                case 1:
+                    BM = new NelderMeadSimplex(Gradient, MaxIter);
+                    MyFuncE = ObjectiveFunction.Value(MinimizePlanc);
+
+                    double W1 = 7000*1e-9, W2 = 14000 * 1e-9;
+                    double T = 50;
+                    W = CreateVector.Dense<double>(N);
+                    double dW = (W2 - W1) / N;
+                    for (int i = 0; i < N; i++)
+                    {
+                        W[i] = W1 + i * dW;
+                    }
+
+                    //sample0 = PlancLaw(W, r.Next(10, 100) - TempK0);
+                    sample0 = PlancLaw(W, T - TempK0);
+                    for (int i = 0; i < N; i++)
+                    {
+                        sample0[i] += 100000*(r.Next(2) - 1.0);
+                    }
+                    BaseVal = CreateVector.DenseOfArray(new double[] { r.Next(10, 100) });
+                    break;
+                default:
+                    BM = new NelderMeadSimplex(Gradient, MaxIter);
+                    MyFuncE = ObjectiveFunction.Value(MinimizeE);
+
+                    double A = 5.1, B = 1.8;
+                    var s0 = new double[N];
+                    var s1 = new double[N];
+                    for (int i = 0; i < N; i++)
+                    {
+                        s0[i] = A + B * i + (r.Next(2) - 1.0) * B / 2.0;
+                        s1[i] = i + (r.Next(2) - 1.0) / 2.0;
+                    }
+                    sample0 = CreateVector.DenseOfArray(s0);
+                    sample1 = CreateVector.DenseOfArray(s1);
+                    BaseVal = CreateVector.DenseOfArray(new double[] { 10, 1 });
+                    break;
+            }
+
             show.addData(sample0.ToArray(), 0);
             try
             {
                 var res = BM.FindMinimum(MyFuncE, BaseVal);
                 var x = res.MinimizingPoint;
-                show.addData((x[1]*sample1+x[0]).ToArray(), 1);
+                switch (cbM.SelectedIndex)
+                {
+                    case 1:
+                        show.addData(PlancLaw(W, x[0] - TempK0).ToArray(), 1);
+                        break;
+                    default:
+                        show.addData((x[1] * sample1 + x[0]).ToArray(), 1);
+                        break;
+                }
                 var sb = new StringBuilder();
                 sb.AppendLine("Iterations " + res.Iterations);
                 sb.AppendLine("Value " + res.FunctionInfoAtMinimum.Value);
                 sb.AppendLine("MinimizingPoint\n" + string.Join("\n", res.MinimizingPoint));
-                sb.AppendLine("\nFunctionInfoAtMinimum\n" + string.Join("\n", res.FunctionInfoAtMinimum.Point));
+                //sb.AppendLine("\nFunctionInfoAtMinimum\n" + string.Join("\n", res.FunctionInfoAtMinimum.Point));
 
                 Results = sb.ToString();
             }
@@ -120,22 +166,30 @@ namespace TestFit
 
         public Vector<double> MinimizeG(Vector<double> v)
         {
-            return v;
-            //return v*0.9;
-            //return CreateVector.DenseOfArray(new double[] { v[0]*v[0] });
-            //return CreateVector.DenseOfArray(new double[] { v[0] });
-            //return CreateVector.DenseOfArray(new double[] { v[0], v[1] });
+            return v;//TODO:Compute gradient function
         }
         public double MinimizeE(Vector<double> x)
         {
             return (sample0 - sample1 * x[1] - x[0]).PointwisePower(2).Sum();
         }
+        public double MinimizePlanc(Vector<double> x)
+        {
+            sample1 = PlancLaw(W, x[0] - TempK0);
+            return (sample0 - sample1).PointwisePower(2).Sum();
+        }
 
 
+        public double PlancLaw(double y, double T)
+        {
+            return 2 * h * Math.Pow(c0, 2) / Math.Pow(y, 5) / (Math.Exp(h * c0 / kb / y / T) - 1); //W/(m**2-um)
+        }
+        public Vector<double> PlancLaw(Vector<double> y, double T)
+        {
+            var c3 = c2 / T;
+            return c1 / y.PointwisePower(5) / ((c3 / y).PointwiseExp() - 1); //W/(m**2-um)
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //BM = new ConjugateGradientMinimizer(Gradient, MaxIter);
-            BM = new NelderMeadSimplex(Gradient, MaxIter);
             show.Clear();
             StartFit();
         }
